9c9b73ee203662743035ad011b04985d1cb50ab6
Kiran Bandla <kbandla@in2void.com>
2011-07-13 11:22:54 -0400

    Updated URL

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(  name='amazon-ses',
         description = 'Python API for Amazon Simple Email Service',
         author= 'Vladimir Pankratiev',
         url = 'http://tagmask.com/vladimir/profile',
-        download_url = 'https://github.com/kbandla/python-amazon-ses-api',
+        download_url = 'https://github.com/pankratiev/python-amazon-ses-api',
         platforms = ['any'],
         py_modules = ['amazon_ses'] 
         )
